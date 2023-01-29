436ecef625e55759ddd5eff40a808dd2a6698d40
Hailey Stewart <kickinhailey@icloud.com>
2023-01-29 14:50:21 -0500

    development - added rectangle

--- Github_Exercise/ContentView.swift
+++ Github_Exercise/ContentView.swift
@@ -16,6 +16,7 @@ struct ContentView: View {
             Text("V. 1.0")
                 .font(.largeTitle)
                 .fontWeight(.black)
+            Rectangle()
         }
         .padding()
         .foregroundColor(.green)

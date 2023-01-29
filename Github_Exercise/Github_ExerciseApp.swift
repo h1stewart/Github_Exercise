6004f13f5df5e7b024aaac439d970b2f01610a81
Hailey Stewart <kickinhailey@icloud.com>
2023-01-29 14:30:39 -0500

    Initial Commit

---
+++ Github_Exercise/Github_ExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Github_ExerciseApp.swift
+//  Github_Exercise
+//
+//  Created by Hailey Stewart on 1/29/23.
+//
+
+import SwiftUI
+
+@main
+struct Github_ExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
